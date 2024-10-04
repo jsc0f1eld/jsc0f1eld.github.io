a3adbc2d776b68362b5afbf6d1b311cb8c84536a
John Miller Ungos <144242441+jsc0f1eld@users.noreply.github.com>
2024-10-04 20:59:30 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,173 @@
+export const bio = [
+  "Hello! I'm John Miller Ungos, a passionate full-stack developer with a strong foundation in building efficient and scalable websites and applications.",
+  "I am committed to the principles of collaboration and continuous skill enhancement, consistently striving to contribute to team success. By actively participating in code reviews and tackling complex projects, I aim to foster a positive and productive development environment that encourages innovation and supports collective achievements.",
+  "Thanks for dropping by!",
+];
+
+export const skills = [
+  {
+    title: "Languages",
+    skillName: "HTML5, CSS, Java, Python, Kotlin",
+    color: "4",
+    percentage: "80",
+  },
+  {
+    title: "Scripting Languages",
+    skillName: "PHP, Typescript, JavaScript",
+    color: "1",
+    percentage: "70",
+  },
+  {
+    title: "Tools",
+    skillName: "Debugging, Chrome DevTools",
+    color: "5",
+    percentage: "80",
+  },
+  {
+    title: "Version Control",
+    skillName: "GitHub, Sourcetree, Bitbucket",
+    color: "7",
+    percentage: "90",
+  }
+];
+
+export const projects = {
+  webProjects: [
+//    {
+//      projectName: "",
+//      image: "",
+//      summary:
+//        "Project Description",
+//      preview: "",
+//      techStack: ["One", "Two", "Three"],
+//    },
+  ],
+  softwareProjects: [
+//    {
+//      projectName: "",
+//      image: "",
+//      summary:
+//        "Project Description",
+//      preview: "",
+//      techStack: ["One", "Two", "Three"],
+//    },
+  ],
+  androidProjects: [
+//    {
+//      projectName: "",
+//      image: "",
+//      summary:
+//        "Project Description",
+//      preview: "",
+//      techStack: ["One", "Two", "Three"],
+//    },
+  ],
+  freelanceProjects: [    
+//    {
+//      projectName: "",
+//      image: "",
+//      summary:
+//        "Project Description",
+//      preview: "",
+//      techStack: ["One", "Two", "Three"],
+//    },
+  ],
+};
+
+export const experience = [
+//  {
+//    title: "Company Name",
+//    duration: "Start Date - End Date",
+//   subtitle: "Job Title",
+//    details: [
+//      `Job Description`,
+//    ],
+//    tags: ["One", "Two", "Three"],
+//    icon: "Icon",
+//  },
+//  {
+//    title: "Accenture",
+//    duration: "Jan 2019 - Feb 2020",
+//    subtitle: "Intern",
+//    details: [
+//      `Developed a application for accenture`,
+//    ],
+//    tags: ["Kotlin", "Java", "XML"],
+//    icon: "group",
+//  },
+];
+
+export const education = [
+  {
+    title: "Bachelor of Science in Information Technology",
+    duration: "",
+    subtitle: "PHINMA - University of Pangasinan",
+    details: [],
+    tags: ["Data Structures & Algorithms", "Operating Systems", "Computer Networks", "Object-oriented Programming", "Project Management", "Network Security"],
+    icon: "graduation-cap",
+  },
+  {
+    title: "Class 12th in STEM",
+    duration: "",
+    subtitle: "St. Albert the Great School",
+    details: [],
+    tags: ["Science", "Technology", "Engineering", "Mathematics"],
+    icon: "book",
+  },
+];
+
+export const footer = [
+//   {
+//     label: "Dev Profiles",
+//     data: [
+//       {
+//         text: "Stackoverflow",
+//         link: "https://stackoverflow.com/users/27133807/jsc0f1eld",
+//       },
+//       {
+//         text: "GitHub",
+//         link: "https://github.com/jsc0f1eld",
+//       },
+//       {
+//         text: "LeetCode",
+//         link: "https://leetcode.com/u/jsc0f1eld/",
+//       },
+//     ],
+//   },
+//   {
+//     label: "Resources",
+//     data: [
+//       {
+//         text: "Enable Dark/Light Mode",
+//         func: "enableDarkMode()",
+//       },
+//       {
+//         text: "Print this page",
+//         func: "window.print()",
+//       },
+//       {
+//         text: "Clone this page",
+//         link: "https://github.com/jsc0f1eld/jsc0f1eld.github.io",
+//       },
+//     ],
+//   },
+  {
+    label: "Links",
+    data: [
+      {
+        text: "LinkedIn",
+        link: "https://www.linkedin.com/in/jsc0f1eld/",
+      },
+      {
+        text: "GitHub",
+        link: "https://github.com/jsc0f1eld",
+      },
+    ],
+  },
+  {
+    label: "copyright-text",
+    data: [
+      "&copy; John Miller Ungos ︱ All Rights Reserved",
+    ],
+  },
+];
